cf3f013c062c1a0f8a80f0a0a0544ede257c3fe7
Bread <withbbang@gmail.com>
2020-09-18 01:11:23 +0900

    Updates

---
+++ precache-manifest.5ec2e5465669d593f1a937058cade6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd426c167e6a449fd069690af2b6cdff",
+    "url": "/bwitter/index.html"
+  },
+  {
+    "revision": "4f73e181cc69d7c1ff76",
+    "url": "/bwitter/static/css/main.27184c10.chunk.css"
+  },
+  {
+    "revision": "ca9817c97e94c099b1fd",
+    "url": "/bwitter/static/js/2.e77843bf.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/bwitter/static/js/2.e77843bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f73e181cc69d7c1ff76",
+    "url": "/bwitter/static/js/main.16fcf329.chunk.js"
+  },
+  {
+    "revision": "fa36b3fa77c05be71140",
+    "url": "/bwitter/static/js/runtime-main.30e9bc64.js"
+  }
+]);
