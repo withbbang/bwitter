471b2c79dd8e8f16effd43943df50c146d65c170
Bread <withbbang@gmail.com>
2020-09-23 23:58:35 +0900

    commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { dbService } from "fbase";
+import { authService, dbService } from "fbase";
 import Bweet from "components/Bweet";
 import BweetFactory from "components/BweetFactory";
 
 const Home = ({ userObj }) => {
   const [bweets, setBweets] = useState([]);
+
   const bweetObj = {
     id: Date.now(),
     text: "이상한거 올리면 지울거임",
@@ -12,6 +13,11 @@ const Home = ({ userObj }) => {
     attachmentUrl:
       "https://firebasestorage.googleapis.com/v0/b/bwitter-4a489.appspot.com/o/angry%20frog.jpg?alt=media&token=2c02adfd-1c2f-45c8-9009-058c6a8d912b",
   };
+
+  // const ownerName = async () => {
+  //   const auths = await authService.listUsers();
+  //   auths.map((auth) => console.log("auth : ", auth));
+  // };
 
   useEffect(() => {
     dbService.collection("bweets").onSnapshot((snapshot) => {
@@ -26,12 +32,13 @@ const Home = ({ userObj }) => {
   return (
     <div>
       <BweetFactory userObj={userObj} />
+      {/* {ownerName()} */}
       <div>
         <Bweet
           bweetObj={bweetObj}
           isOwner={false}
           isNoti={true}
-          username={"- GOD -"}
+          username={"Administer"}
         />
         {bweets.map((bweet) => (
           <Bweet
